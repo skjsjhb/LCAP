66a78daf3bd96038a0ddcea5784d5200b58128a7
skjsjhb <skjsjhb@outlook.com>
2025-07-22 22:50:56 +0800

    feat(*): drop tauri stack, move to saucer

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,21 @@
 use std::env;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
-use std::process::ExitCode;
 use std::str::FromStr;
+use std::sync::Arc;
 use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 
 use gumdrop::Options;
-use tao::dpi::PhysicalSize;
-use tao::event::Event;
-use tao::event::WindowEvent;
-use tao::event_loop::ControlFlow;
-use tao::event_loop::EventLoop;
-use tao::event_loop::EventLoopBuilder;
-use tao::event_loop::EventLoopProxy;
-use tao::platform::run_return::EventLoopExtRunReturn;
-use tao::window::Window;
-use tao::window::WindowBuilder;
-use wry::WebContext;
-use wry::WebViewBuilder;
+use saucers::app::App;
+use saucers::options::AppOptions;
+use saucers::prefs::Preferences;
+use saucers::webview::events::LoadEvent;
+use saucers::webview::events::NavigateEvent;
+use saucers::webview::Webview;
 
 #[derive(Options)]
 struct LandingArgs {
@@ -59,19 +54,9 @@ struct LandingArgs {
     wait_timeout: u64
 }
 
-struct WebViewInit {
-    show_on_init: bool,
-    context: Option<WebContext>
-}
-
-enum LandingEvents {
-    Close(i32),
-    SetVisible
-}
-
 const DEFAULT_URL: &str = "https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&response_type=code&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf";
 
-fn main() -> ExitCode {
+fn main() {
     let args = LandingArgs::parse_args_default_or_exit();
 
     let part_id = args
@@ -84,184 +69,91 @@ fn main() -> ExitCode {
     let code_tag = args.code_tag;
     let error_tag = args.error_tag;
 
-    let mut events: EventLoop<LandingEvents> = EventLoopBuilder::with_user_event().build();
-    let proxy = events.create_proxy();
+    let cache_root = get_cache_root(&part_id);
+    let show_now = !is_likely_auto_login(cache_root.as_path());
 
-    let WebViewInit {
-        show_on_init: should_show_now,
-        mut context
-    } = prepare_webview(&part_id);
+    let (_cc, app) = App::new(AppOptions::new("LCAP"));
 
-    let window = WindowBuilder::new()
-        .with_title(args.title)
-        .with_visible(should_show_now)
-        .build(&events)
-        .expect("Failed to create window");
+    let mut prefs = Preferences::new(&app);
+    prefs.set_storage_path(cache_root.to_str().unwrap());
 
-    set_optimal_window_size(&window);
+    let webview = Arc::new(Webview::new(&prefs).unwrap());
+    let size = optimal_window_size();
+    webview.set_size(size.0, size.1);
 
-    let mut wb = match context.as_mut() {
-        Some(wc) => WebViewBuilder::with_web_context(wc),
-        None => WebViewBuilder::new()
-    };
-
-    #[cfg(target_os = "macos")]
-    {
-        use wry::WebViewBuilderExtDarwin;
-        wb = wb.with_data_store_identifier(part_id.as_bytes().to_owned());
-    }
-
-    if !should_show_now {
+    if !show_now {
         // Set triggers (timeout, page loading) if the window is not known to be visible at creation
         thread::spawn({
-            let proxy = proxy.clone();
+            let webview = Arc::downgrade(&webview);
             let timeout = args.wait_timeout;
             move || {
                 sleep(Duration::from_millis(timeout));
-                let _ = proxy.send_event(LandingEvents::SetVisible);
+                if let Some(webview) = webview.upgrade() {
+                    webview.show();
+                }
             }
         });
 
-        let on_page_loaded = {
-            let proxy = proxy.clone();
-
-            move |_, _| {
-                let _ = proxy.send_event(LandingEvents::SetVisible);
-            }
-        };
-
-        wb = wb.with_on_page_load_handler(on_page_loaded);
+        webview.once::<LoadEvent>(Box::new(|w, _| w.show()));
+    } else {
+        webview.show();
     }
 
-    let on_url_captured = create_url_captured_handler(
-        proxy.to_owned(),
-        UrlHandlerContext {
-            file_path: args.file.to_owned(),
-            code_tag,
-            error_tag
-        }
-    );
+    let file_path = args.file;
 
-    wb = wb.with_url(url).with_navigation_handler(on_url_captured);
+    webview.set_url(url);
 
-    #[cfg(target_os = "linux")]
-    let view_res = {
-        use tao::platform::unix::WindowExtUnix;
-        use wry::WebViewBuilderExtUnix;
-
-        if let Some(vbox) = window.default_vbox() {
-            wb.build_gtk(vbox)
-        } else {
-            wb.build_gtk(window.gtk_window())
-        }
-    };
-
-    #[cfg(not(target_os = "linux"))]
-    let view_res = wb.build(&window);
-
-    // The webview instance must be hold here, or it will be destroyed
-    let _view = view_res.expect("Unable to create webview instance");
-
-    let rc = events.run_return(|ev, _, control| {
-        *control = ControlFlow::Wait;
-
-        match ev {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control = ControlFlow::ExitWithCode(1),
-
-            Event::UserEvent(LandingEvents::Close(ec)) => *control = ControlFlow::ExitWithCode(ec),
-
-            Event::UserEvent(LandingEvents::SetVisible) => window.set_visible(true),
-
-            _ => {}
-        }
-    });
-
-    ExitCode::from(rc as u8)
-}
-
-struct UrlHandlerContext {
-    file_path: Option<String>,
-    code_tag: String,
-    error_tag: String
-}
-
-fn create_url_captured_handler(
-    proxy: EventLoopProxy<LandingEvents>,
-    ctx: UrlHandlerContext
-) -> impl Fn(String) -> bool {
-    let UrlHandlerContext {
-        file_path,
-        code_tag,
-        error_tag
-    } = ctx;
-
-    let dump_output = move |s: String| {
-        match file_path {
-            Some(ref fp) => {
-                File::create(fp)
-                    .and_then(|mut f| f.write_all(s.as_bytes()))
-                    .expect("Failed to write to specified file");
-            }
-            None => {
-                println!("\n{s}\n")
-            }
-        };
-    };
-
-    move |s: String| {
-        let Ok(u) = url::Url::from_str(&s) else {
+    webview.on::<NavigateEvent>(Box::new(move |w, nav| {
+        let Ok(u) = url::Url::from_str(&nav.url()) else {
             return true;
         };
 
+        let mut output = None;
+
         if let Some(ep) = u.query_pairs().find(|it| it.0 == error_tag) {
-            dump_output(format!("LCAP:ERR={}", ep.1));
-            let _ = proxy.send_event(LandingEvents::Close(1));
-            return false; // No need to navigate further
+            output = Some(format!("LCAP:ERR={}", ep.1));
         }
 
         if let Some(cp) = u.query_pairs().find(|it| it.0 == code_tag) {
-            dump_output(format!("LCAP:CODE={}", cp.1));
-            let _ = proxy.send_event(LandingEvents::Close(0));
-            return false;
+            output = Some(format!("LCAP:CODE={}", cp.1));
         }
 
-        true
-    }
-}
-
-fn set_optimal_window_size(window: &Window) {
-    if let Some(mn) = window.current_monitor() {
-        let (w, h): (u32, u32) = mn.size().into();
-
-        fn scale(a: u32, f: f32) -> u32 { ((a as f32) * f).round() as u32 }
-
-        window.set_inner_size(PhysicalSize::new(scale(w, 0.6), scale(h, 0.6)));
-    }
-}
-
-fn prepare_webview(uuid: &uuid::Uuid) -> WebViewInit {
-    #[cfg(target_os = "macos")]
-    return WebViewInit {
-        show_on_init: false,
-        context: None
-    };
-
-    #[cfg(not(target_os = "macos"))]
-    {
-        let cache_root = get_cache_root(uuid);
-        let show_now = match cache_root.try_exists() {
-            Ok(ex) => !ex,
-            Err(_) => true
+        let Some(output) = output else {
+            return true;
         };
 
-        WebViewInit {
-            show_on_init: show_now,
-            context: Some(WebContext::new(Some(cache_root)))
-        }
+        match file_path {
+            Some(ref fp) => {
+                File::create(fp)
+                    .and_then(|mut f| f.write_all(output.as_bytes()))
+                    .expect("Failed to write to specified file");
+            }
+            None => {
+                println!("\n{output}\n")
+            }
+        };
+        w.close();
+
+        false
+    }));
+
+    app.run();
+}
+
+fn optimal_window_size() -> (i32, i32) {
+    let (w, h) = screen_size::get_primary_screen_size().unwrap_or((1920u64, 1080u64));
+
+    ((w as f32 * 0.8) as i32, (h as f32 * 0.8) as i32)
+}
+
+fn is_likely_auto_login(cache: &Path) -> bool {
+    #[cfg(target_os = "macos")]
+    {
+        false
     }
+
+    #[cfg(not(target_os = "macos"))]
+    cache.try_exists().is_ok_and(|it| it)
 }
 
 fn get_cache_root(uuid: &uuid::Uuid) -> PathBuf {
